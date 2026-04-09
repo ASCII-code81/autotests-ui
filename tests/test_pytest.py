4ef27a78372e193a1a8e2c243a0529239ee20c5d
ASCII-code81 <katedark2720@gmail.com>
2026-04-09 13:57:37 +0300

    Интеграция Pytest и Playwright

--- tests/test_pytest.py
+++ tests/test_pytest.py
@@ -1,7 +1,6 @@
 def test_user_login():
     print('Hello!')
 
-
 class TestUserLogin:
     def test_1(self):
         ...
@@ -13,5 +12,6 @@ def test_assert_positive_case():
     assert (2+2) == 4
     assert (3+3) == 6
     assert (4+4) == 8
+
 def test_assert_negative_case():
-    assert (2+2)==5, "(2+2) != 5"
+    assert (2+2) == 5, "(2+2) != 5"
