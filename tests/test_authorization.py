8986837204a6556a19a5df30977a15c7cdadb09e
ASCII-code81 <katedark2720@gmail.com>
2026-04-19 13:31:21 +0300

    Знакомство с PageFactory

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,14 +1,13 @@
-from playwright.sync_api import expect, sync_playwright, Page
 import pytest 
-from pages.login_page import LoginPage
+from pages.login_page import LoginPage 
+
 
 @pytest.mark.regression
 @pytest.mark.authorization
-@pytest.mark.parametrize('email, password', [("user.name@gmail.com", "password"), ("user.name@gmail.com", "  "), ("  ", "password")])
-def test_wrong_email_or_password_authorization(chromium_page: Page, email: str, password: str):
-        login_page = LoginPage(page=chromium_page)
+@pytest.mark.parametrize('email, password', [("user.name@gmail.com", "password"), ("user.name@gmail.com", "sadas"), ("asdas", "password")])
+def test_wrong_email_or_password_authorization(login_page: LoginPage, email: str, password: str):
         login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
-        login_page.fill_login_form(email=email, password=password)
+        login_page.login_form.fill(email=email, password=password)
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
